11d13b2619cde2419fdd140661bba91b1d7f0aa5
jomc <jomc@cm058-00.dhcp.unc.edu>
2018-11-14 12:38:09 -0500

    First commit, set up the files

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(function(){
+  console.log('scripts loaded');
+
+var myKey = config.MY_KEY;
+var url = 'https://newsapi.org/v2/everything?q=bitcoin&from=2018-10-14&sortBy=publishedAt&apiKey=' + myKey;
+
+
+
+});
